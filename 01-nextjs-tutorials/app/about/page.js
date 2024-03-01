1a75aad52337a9a0baab7f02c5fb57a1c5eb5406
Sumit Paul <pauls1317@gmail.com>
2024-03-01 13:46:30 +0530

    Link component from next/link

--- 01-nextjs-tutorials/app/about/page.js
+++ 01-nextjs-tutorials/app/about/page.js
@@ -1,7 +1,11 @@
+import Link from "next/link"
+
 const AboutPage = () => {
   return (
     <div>
         <h1 className="text-7xl">AboutPage</h1>
+
+        <Link href="/" className="text-2xl">Home Page</Link>
     </div>
   )
 }
--- 01-nextjs-tutorials/app/page.js
+++ 01-nextjs-tutorials/app/page.js
@@ -1,7 +1,11 @@
+import Link from "next/link"
+
 const HomePage = () => {
   return (
     <div>
       <h1 className="text-7xl">Home Page</h1>
+
+      <Link href="about" className="text-2xl">About Page</Link>
     </div>
   )
 }
