4558741782f19ac27005d52cd9252ab485895d47
Sumit Paul <pauls1317@gmail.com>
2024-06-06 23:18:50 +0530

    added navbar to root layout

---
+++ 01-nextjs-tutorials/components/Navbar.jsx
@@ -0,0 +1,31 @@
+import Link from 'next/link';
+
+const links = [
+  { href: '/client', label: 'client' },
+  { href: '/drinks', label: 'drinks' },
+  { href: '/tasks', label: 'tasks' },
+  { href: '/query', label: 'query' },
+];
+const Navbar = () => {
+  return (
+    <nav className="bg-base-300 py-4">
+      <div className="navbar px-8 max-w-6xl mx-auto flex-col sm:flex-row">
+        <Link href="/" className="btn btn-primary">
+          Next.js
+        </Link>
+        <ul className="menu menu-horizontal md:ml-8">
+          {links.map(link => {
+            return (
+              <li key={link.href}>
+                <Link href={link.href} className="capitalize">
+                  {link.label}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+export default Navbar;
