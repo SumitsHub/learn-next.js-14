ea560ac667d5d80cbf68ddf5ffdc40971956ebc8
Sumit Paul <pauls1317@gmail.com>
2024-06-06 23:19:18 +0530

    styled landing page using tailwind css classes

--- 01-nextjs-tutorials/app/page.js
+++ 01-nextjs-tutorials/app/page.js
@@ -3,10 +3,11 @@ import Link from "next/link"
 const HomePage = () => {
   return (
     <div>
-      <h1 className="text-7xl">Home Page</h1>
-
-      <Link href="about" className="text-2xl">About Page</Link>
-    </div>
+    <h1 className='text-5xl mb-8 font-bold'>Next.js Tutorial</h1>
+    <Link href='/client' className='btn btn-accent'>
+      get started
+    </Link>
+  </div>
   )
 }
 export default HomePage
