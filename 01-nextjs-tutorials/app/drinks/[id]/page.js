1acf5d801d0ba8c46fde90e08a084ebf219726d2
Sumit Paul <pauls1317@gmail.com>
2024-06-10 22:58:37 +0530

    show local and remote images with Next's Image component

--- 01-nextjs-tutorials/app/drinks/[id]/page.js
+++ 01-nextjs-tutorials/app/drinks/[id]/page.js
@@ -1,10 +1,12 @@
 import Link from "next/link";
+import drinkImg from "./drink.jpg";
+import Image from "next/image";
 
-const url = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=';
+const url = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=";
 
 const getSingleDrink = async id => {
   const res = await fetch(url + id);
-  if (!res.ok) throw new Error('Failed to fetch drink!');
+  if (!res.ok) throw new Error("Failed to fetch drink!");
   const data = await res.json();
   return data;
 };
@@ -19,6 +21,19 @@ async function SingleDrinkPage({ params }) {
       <Link href="/drinks" className="btn btn-primary mt-8 mb-12">
         back to drinks
       </Link>
+      {/* Local images -  */}
+      {/* <img src={drinkImg.src} alt="s" className='w-48 h-48 rounded' /> */}
+      {/* <Image src={drinkImg} className='w-48 h-48 rounded' alt='' /> */}
+
+      {/* Remote image */}
+      <Image
+        src={imgSrc}
+        height={300}
+        width={300}
+        className="h-48 w-48 rounded-lg shadow-lg"
+        priority
+        alt={title}
+      />
       <h1 className="text-4xl mb-8">{title}</h1>
     </div>
   );
