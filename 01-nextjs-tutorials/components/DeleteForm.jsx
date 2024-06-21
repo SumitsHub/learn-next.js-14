bc76cc620bd60c84db33c23660a285d9c051a4a0
Sumit Paul <pauls1317@gmail.com>
2024-06-21 09:36:14 +0530

    pending state for delete form

--- 01-nextjs-tutorials/components/DeleteForm.jsx
+++ 01-nextjs-tutorials/components/DeleteForm.jsx
@@ -1,10 +1,21 @@
+"use client";
 import { deleteTask } from "@/utils/actions";
+import { useFormStatus } from "react-dom";
+
+const DeleteBtn = () => {
+  const { pending } = useFormStatus();
+  return (
+    <button className="btn btn-xs btn-error" disabled={pending}>
+      {pending ? "pending..." : "delete"}
+    </button>
+  );
+};
 
 const DeleteForm = ({ id }) => {
   return (
     <form action={deleteTask}>
       <input type="hidden" name="id" value={id} />
-      <button className="btn btn-xs btn-error">delete</button>
+      <DeleteBtn />
     </form>
   );
 };
