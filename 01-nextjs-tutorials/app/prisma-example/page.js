4a46ee93532e2f279cb545db12d174ee34516071
Sumit Paul <pauls1317@gmail.com>
2024-06-25 11:27:52 +0530

    local build changes

--- 01-nextjs-tutorials/app/prisma-example/page.js
+++ 01-nextjs-tutorials/app/prisma-example/page.js
@@ -1,30 +1,38 @@
 import prisma from "@/utils/db";
 
 const prismaHandlers = async () => {
-  await prisma.task.create({
-    data: {
-      content: 'Wake up',
-      completed: Math.random()*10 < 5 ? true : false
-    }
-  });
+  console.log("prisma example");
+  // await prisma.task.create({
+  //   data: {
+  //     content: 'Wake up',
+  //     completed: Math.random()*10 < 5 ? true : false
+  //   }
+  // });
 
   const allTasks = await prisma.task.findMany({
     orderBy: {
-      createdAt: 'desc'
-    }
+      createdAt: "desc",
+    },
   });
 
   return allTasks;
-}
+};
 
 async function PrismaExample() {
   const tasks = await prismaHandlers();
-
-  return <div>
-    <h1 className="text-7xl">Prisma Examples</h1>
-    {tasks.map((task)=>{
-      return <h2 className="text-xl py-2" key={task.id}>{task.content} {task.completed && "✔️"}</h2>
-    })}
-  </div>;
+  if (tasks.length === 0)
+    return <h2 className="mt-8 font-medium text-lg">No tasks to show...</h2>;
+  return (
+    <div>
+      <h1 className="text-7xl">Prisma Examples</h1>
+      {tasks.map(task => {
+        return (
+          <h2 className="text-xl py-2" key={task.id}>
+            {task.content} {task.completed && "✔️"}
+          </h2>
+        );
+      })}
+    </div>
+  );
 }
 export default PrismaExample;
