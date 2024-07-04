5ec8080ff7968b4e55441c68adef916ca306d1eb
Sumit Paul <pauls1317@gmail.com>
2024-07-04 22:27:54 +0530

    providers component

---
+++ 02-gptgenius/app/providers.jsx
@@ -0,0 +1,11 @@
+import { Toaster } from "react-hot-toast";
+
+const Providers = ({ children }) => {
+  return (
+    <>
+      <Toaster position="top-center" />
+      {children}
+    </>
+  );
+};
+export default Providers;
