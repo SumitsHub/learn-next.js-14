01cafe3936e49e846c23171d02d3d102a4bd0d96
Sumit Paul <sumitpaul.works@gmail.com>
2024-07-08 18:22:33 +0530

    new tour page and component

---
+++ 02-gptgenius/app/(dashboard)/tours/new-tour/page.js
@@ -0,0 +1,16 @@
+import NewTour from "@/components/NewTour";
+import {
+  HydrationBoundary,
+  QueryClient,
+  dehydrate,
+} from "@tanstack/react-query";
+
+const NewTourPage = () => {
+  const queryClient = new QueryClient();
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <NewTour />
+    </HydrationBoundary>
+  );
+};
+export default NewTourPage;
---
+++ 02-gptgenius/components/NewTour.jsx
@@ -0,0 +1,23 @@
+'use client';
+
+const NewTour = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const destination = Object.fromEntries(formData.entries());
+    console.log(destination);
+  };
+  return (
+    <>
+      <form onSubmit={handleSubmit} className='max-w-2xl'>
+        <h2 className="mb-4">Select your dream destination</h2>
+        <div className="join w-full">
+            <input type="text" className="input input-bordered join-item w-full" placeholder='city' name='city' required/>
+            <input type="text" className="input input-bordered join-item w-full" placeholder='country' name='country' required/>
+            <button className='btn btn-primary join-item' type='submit'>generate tour</button>
+        </div>
+      </form>
+    </>
+  );
+};
+export default NewTour;
