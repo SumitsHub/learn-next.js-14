7f83621cabc0de096fbf5f375aca384708156925
Sumit Paul <sumitpaul.works@gmail.com>
2024-07-08 15:25:54 +0530

    Chat page complete

--- 02-gptgenius/components/Chat.jsx
+++ 02-gptgenius/components/Chat.jsx
@@ -2,24 +2,48 @@
 import { generateChatResponse } from "@/utils/action";
 import { useMutation } from "@tanstack/react-query";
 import { useState } from "react";
+import toast from "react-hot-toast";
 
 const Chat = () => {
   const [text, setText] = useState("");
   const [messages, setMessages] = useState([]);
-    const { mutate } = useMutation({
-      mutationFn: msg => generateChatResponse(msg),
-    });
+  const { mutate, isPending } = useMutation({
+    mutationFn: (msg) => generateChatResponse([...messages, msg]),
+    onSuccess: (data) => {
+      if (!data) {
+        toast.error("Something went wrong...");
+        return;
+      }
+      setMessages((prev) => [...prev, data]);
+    },
+  });
 
-    const handleSubmit = e => {
-      e.preventDefault();
-
-      mutate(text);
-    };
-
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const query = { role: "user", content: text };
+    mutate(query);
+    setMessages((prev) => [...prev, query]);
+    setText("");
+  };
+  // console.log(messages);
   return (
     <div className="min-h-[calc(100vh-6rem)] grid grid-rows-[1fr,auto]">
       <div>
-        <h2 className="text-5xl">messages</h2>
+        {messages.map(({ role, content }, index) => {
+          const avatar = role === "user" ? "👤" : "🤖";
+          const bcg = role === "user" ? "bg-base-200" : "bg-base-100";
+
+          return (
+            <div
+              key={index}
+              className={`${bcg} flex py-6 -mx-8 px-8 text-xl leading-loose border-b border-base-300`}
+            >
+              <span className="mr-4">{avatar}</span>
+              <p className="max-w-3xl">{content}</p>
+            </div>
+          );
+        })}
+        {isPending ? <span className="loading"></span> : null}
       </div>
       <form onSubmit={handleSubmit} className="max-w-4xl pt-12">
         <div className="join w-full">
@@ -29,10 +53,14 @@ const Chat = () => {
             className="input input-bordered join-item w-full"
             value={text}
             required
-            onChange={e => setText(e.target.value)}
+            onChange={(e) => setText(e.target.value)}
           />
-          <button className="btn btn-primary join-item" type="submit">
-            ask question
+          <button
+            className="btn btn-primary join-item"
+            type="submit"
+            disabled={isPending}
+          >
+            {isPending ? "please wait" : "ask question"}
           </button>
         </div>
       </form>
--- 02-gptgenius/utils/action.js
+++ 02-gptgenius/utils/action.js
@@ -1,6 +1,25 @@
 "use server";
 
-export const generateChatResponse = async chatMessage => {
-  console.log(chatMessage);
-  return "awesome";
+import OpenAI from "openai";
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY
+})
+
+export const generateChatResponse = async chatMessages => {
+  // console.log(chatMessages);
+  try {
+    const response = await openai.chat.completions.create({
+      messages: [
+        {role: 'system', content: 'you are a helpful assistant'},
+        ...chatMessages
+      ],
+      model: 'gpt-3.5-turbo',
+      temperature: 0
+    });
+
+    return response.choices[0].message;
+  } catch (error) {
+    return null;
+  }
 };
