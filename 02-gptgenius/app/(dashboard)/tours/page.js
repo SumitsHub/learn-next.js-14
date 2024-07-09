2a0c1e118e90105fea8d50f9516a3a756dcd61ed
Sumit Paul <sumitpaul.works@gmail.com>
2024-07-09 17:59:52 +0530

    single tour

--- 02-gptgenius/app/(dashboard)/tours/page.js
+++ 02-gptgenius/app/(dashboard)/tours/page.js
@@ -4,14 +4,14 @@ import {
   HydrationBoundary,
   QueryClient,
 } from '@tanstack/react-query';
-import { getAllTours } from '@/utils/actions';
+import { getAllTours } from '@/utils/action';
 export default async function AllToursPage() {
   const queryClient = new QueryClient();
   await queryClient.prefetchQuery({
     queryKey: ['tours'],
     queryFn: () => getAllTours(),
   });
-  
+
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
       <ToursPage />
--- 02-gptgenius/utils/action.js
+++ 02-gptgenius/utils/action.js
@@ -109,3 +109,11 @@ export const getAllTours = async (searchTerm) => {
   });
   return tours;
 };
+
+export const getSingleTour = async (id) => {
+  return prisma.tour.findUnique({
+    where: {
+      id,
+    },
+  });
+};
