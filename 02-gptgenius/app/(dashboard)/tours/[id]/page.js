7c23fbedc6c4cc550223080c16488a73cec4f948
Sumit Paul <sumitpaul.works@gmail.com>
2024-07-09 18:07:23 +0530

    generate image using openAI

--- 02-gptgenius/app/(dashboard)/tours/[id]/page.js
+++ 02-gptgenius/app/(dashboard)/tours/[id]/page.js
@@ -1,17 +1,34 @@
-import TourInfo from '@/components/TourInfo';
-import { getSingleTour } from '@/utils/action';
-import Link from 'next/link';
-import { redirect } from 'next/navigation';
+import TourInfo from "@/components/TourInfo";
+import { generateTourImage, getSingleTour } from "@/utils/action";
+import Image from "next/image";
+import Link from "next/link";
+import { redirect } from "next/navigation";
 const SingleTourPage = async ({ params }) => {
   const tour = await getSingleTour(params.id);
   if (!tour) {
-    redirect('/tours');
+    redirect("/tours");
   }
+  const tourImage = await generateTourImage({
+    city: tour.city,
+    country: tour.country,
+  });
   return (
     <div>
-      <Link href='/tours' className='btn btn-secondary mb-12'>
+      <Link href="/tours" className="btn btn-secondary mb-12">
         back to tours
       </Link>
+      {tourImage ? (
+        <div>
+          <Image
+            src={tourImage}
+            width={300}
+            height={300}
+            className="rounded-xl shadow-xl mb-16 h-96 w-96 object-cover"
+            alt={tour.title}
+            priority
+          />
+        </div>
+      ) : null}
       <TourInfo tour={tour} />
     </div>
   );
--- 02-gptgenius/utils/action.js
+++ 02-gptgenius/utils/action.js
@@ -117,3 +117,16 @@ export const getSingleTour = async (id) => {
     },
   });
 };
+
+export const generateTourImage = async ({ city, country }) => {
+  try {
+    const tourImage = await openai.images.generate({
+      prompt: `a panoramic view of the ${city} ${country}`,
+      n: 1,
+      size: "512x512",
+    });
+    return tourImage?.data[0]?.url;
+  } catch (error) {
+    return null;
+  }
+};
